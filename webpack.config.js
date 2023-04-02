3ffdc8eb5131a7cb31e9135967fdba0b179b9a51
tontonel <tionut80@gmail.com>
2023-04-03 01:55:28 +0300

    added env for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -9,6 +10,7 @@ module.exports = {
     filename: "bundle.js",
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
