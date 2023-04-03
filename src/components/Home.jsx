90657cd8f8bd5d273c2ddea6326e29760468bb93
tontonel <tionut80@gmail.com>
2023-04-03 23:19:51 +0300

    hope last commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import CarsTable from "./CarsTable/CarsTable";
 import NavBar from "./NavBar";
 import Box from "@mui/material/Box";
-import AddCarButton from "./CarsTable/AddCarButton";
 
 export default function Home() {
     return (
