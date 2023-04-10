6872d820ef370b0f7bbd24fed18cfb6b04ffa2b0
tontonel <tionut80@gmail.com>
2023-04-10 10:57:33 +0300

    remove the proccess env

--- src/api/car/carActions.js
+++ src/api/car/carActions.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
 export const fetchCars = async () => {
-    const result = await axios.get(process.env.API_URL + '/api/car');
+    const result = await axios.get('/api/car');
     return result.data;
 }
 
 export const deleteCar = async (id) => {
-    return await axios.delete(process.env.API_URL + '/api/car/' + id);
+    return await axios.delete('/api/car/' + id);
 }
 
 export const updateCar = async (car) => {
-    return await axios.put(process.env.API_URL + '/api/car/' + car.carId, car);
+    return await axios.put(car.carId, car);
 }
 
 export const createCar = async (car) => {
-    return await axios.post(process.env.API_URL + '/api/car', car);
+    return await axios.post('/api/car', car);
 }
 
 export const filterCars = async (filter) => {
-    const result = await axios.get(process.env.API_URL + '/api/car/filterByYear/' + filter);
+    const result = await axios.get('/api/car/filterByYear/' + filter);
     return result.data;
 }
